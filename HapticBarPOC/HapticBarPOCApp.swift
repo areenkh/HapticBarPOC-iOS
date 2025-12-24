d014c7193d76e493269c438439d2ba2b32477a36
Areen <akhalaila@tulane.edu>
2025-12-24 13:43:27 +0200

    Initial Commit

---
+++ HapticBarPOC/HapticBarPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticBarPOCApp.swift
+//  HapticBarPOC
+//
+//  Created by Khalaila, Areen on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticBarPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
